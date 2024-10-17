df989e835405f38a15b67ff11fa47c9da3e26eca
Shuvojyoti Singha <sjshuvosingha@gmail.com>
2024-10-17 23:31:38 +0300

    Age exercise (all tasks completed)

--- src/App.java
+++ src/App.java
@@ -1,24 +1,37 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        int age = 10;
-        if (age > 0 && age < 18) {
-            System.out.println("You are underage.");
-            if (age >= 15) {
-                System.out.println("You can drive a moped.");
-            }
-        } else if (age >= 65) {
-            System.out.println("You are retired.");
-        }
-
-        else if (age == 18) {
+        int age = 66;
+        
+        // task A
+        if (age == 18) {
             System.out.println("You can drive a car");
         }
-
-        else if (age == 10 || age == 20 || age == 30 || age == 40 || age == 50 || age == 60 || age == 70 || age == 80
-                || age == 90 || age == 100 || age == 110 || age == 120) {
+        // task C
+        else if (age == 100) {
+            System.out.println("Congratulations!");
+            System.out.println("Congratulations!");
+            System.out.println("Congratulations!");
+        }
+        // task D
+        else if (age >= 40 && age <= 50) {
+            System.out.println("Happy mid-life.");
+        
+        }
+        // task B
+         else if (age == 10 || age == 20 || age == 30 || age == 40 || age == 50 || age == 60 || age == 70 || age == 80
+        || age == 90 || age == 100 || age == 110 || age == 120) {
             System.out.println("Anniversary Party!!");
-        } else {
+        }
+        else if (age > 0 && age < 18) {
+            System.out.println("You are underage.");
+        } 
+        else if (age >= 65) {
+            System.out.println("You are retired.");
+        }
+        else if (age >= 15) {
+            System.out.println("You can drive a moped.");
+        }
+        else {
             System.out.println("You are an adult.");
         }
     }
